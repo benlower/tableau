2e471a6f1e217e9a64102014cad1b1c00715ba84
Ben Lower <github@benlower.us>
2015-07-20 11:32:25 -0700

    Update configserver.bat
    adding changes to fix the path to tabcmd.exe and to sign back in to our newly created site

--- tabcmd example/configserver.bat
+++ tabcmd example/configserver.bat
@@ -1,50 +1,66 @@
-REM -- Login to our site
-tabcmd login -s http://localhost -u admin -p admin
+REM -- Make sure our server is started
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabadmin start
 
 
-REM  -- Create a new site called "Acme Site"
-tabcmd createsite "Acme Site"
+REM -- Login to our server
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe login -s http://localhost -u admin -p admin
 
 
-REM -- Include Tableau Sample Data in the site
-REM TODO
+REM  -- Create a new site called "Acme Site" witht the siteID "acme"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsite "Acme Site" -r "acme"
 
 
-REM -- Publish some data sources with some extracts
-REM TODO
+REM -- Log out of our server
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe logout
+
+
+REM -- Log in -> now we want to use our newly created "Acme Site"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe login -s http://localhost --site "acme" -u admin -p admin
+
 
 
 REM -- Create a project for each of our six departments -> HR, IT, Sales, Operations, Billing and Facilities
-tabcmd createproject -n "HR Project" -d "Project for HR department"
-tabcmd createproject -n "IT Project" -d "Project for IT department"
-tabcmd createproject -n "Sales Project" -d "Project for Sales department"
-tabcmd createproject -n "Operations Project" -d "Project for Operations department"
-tabcmd createproject -n "Billing Project" -d "Project for Billing department"
-tabcmd createproject -n "Facilities Project" -d "Project for Facilities department"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createproject -n "HR_Project" -d "Project for HR department"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createproject -n "IT_Project" -d "Project for IT department"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createproject -n "Sales_Project" -d "Project for Sales department"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createproject -n "Operations_Project" -d "Project for Operations department"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createproject -n "Billing_Project" -d "Project for Billing department"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createproject -n "Facilities_Project" -d "Project for Facilities department"
 
 
 REM -- Create a group for each of our six departments -> HR, IT, Sales, Operations, Billing and Facilities
-tabcmd creategroup "HR"
-tabcmd creategroup "IT"
-tabcmd creategroup "Sales"
-tabcmd creategroup "Operations"
-tabcmd creategroup "Billing"
-tabcmd creategroup "Facilities"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe creategroup "HR"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe creategroup "IT"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe creategroup "Sales"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe creategroup "Operations"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe creategroup "Billing"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe creategroup "Facilities"
 
 
-REM -- Give each group permission for its and only its project
-REM TODO
-
+REM -- Create our site users
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsiteusers "c:\temp\HRusers.csv" --role "Publisher"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsiteusers "c:\temp\ITusers.csv" --role "Publisher"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsiteusers "c:\temp\Salesusers.csv" --role "Interactor"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsiteusers "c:\temp\Operationsusers.csv" --role "Interactor"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsiteusers "c:\temp\Billingusers.csv" --role "Interactor"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe createsiteusers "c:\temp\Facilitiesusers.csv" --role "Interactor"
 
 REM -- Create a user for each department called "<dept> Manager" and give manager rights for the project content
 REM -- This requires a CSV file
-tabcmd addusers "HR" --users "HRusers.csv"
-tabcmd addusers "IT" --users "ITusers.csv"
-tabcmd addusers "Sales" --users "Salesusers.csv"
-tabcmd addusers "Operations" --users "Operationsusers.csv"
-tabcmd addusers "Billing" --users "Billingusers.csv"
-tabcmd addusers "Facilities" --users "Facilitiesusers.csv"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe addusers "HR" --users "c:\temp\HRusers.csv"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe addusers "IT" --users "c:\temp\ITusers.csv"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe addusers "Sales" --users "c:\temp\Salesusers.csv"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe addusers "Operations" --users "c:\temp\Operationsusers.csv"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe addusers "Billing" --users "c:\temp\Billingusers.csv"
+c:\PROGRA~2\Tableau\TABLEA~1\9.0\bin\tabcmd.exe addusers "Facilities" --users "c:\temp\Facilitiesusers.csv"
 
+
+REM -- Manual work TODO
+REM -- Give each group permission for its and only its project
+
+REM -- Include Tableau Sample Data in the site <TODO-> download then upload from TD>
+
+REM -- Publish some data sources with some extracts
 
 REM -- Ensure that IT and HR user group can publish new materials
 
@@ -52,12 +68,12 @@ REM -- Make user group for other departments and give them interactor rights
 
 REM -- Exception 1 -> HR MGR sees all IT materials
 
-REM -- Exception 2 -> Sales and IT share a data source – Should be a SQL source that prompts for password
+REM -- Exception 2 -> Sales and IT share a data source – Should be a SQL source that prompts for password  <TODO>
 
 REM -- Exception 3 -> IT Manager can publish in all locations
 
 REM -- Exception 4 -> Facilities can see everything of Billing but Billing cannot see Facilities
 
-REM -- Exception 5 -> Facilities, Billing and IT share a data source
+REM -- Exception 5 -> Facilities, Billing and IT share a data source <TODO>
 
-REM -- Exception 6 -> All Users have a SQL Data Source that does not prompt for password
+REM -- Exception 6 -> All Users have a SQL Data Source that does not prompt for password <TODO>
